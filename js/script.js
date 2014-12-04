5f66dd5b06cfe24c98128cb4572da6a0f0ae70be
Milton Ramirez <milsmsramirezz@gmail.com>
2014-12-04 16:05:28 -0600

    v1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-// Javascript Code.
+$(document).ready(function(){
+  $('.boton').click(function(){
+    var pais = $('input:text[name=nombre]').val();
+    var service_url = 'https://www.googleapis.com/freebase/v1/search';
+    var params = {
+    'query': pais,
+    'limit': 10,
+    'indent': true
+  };
+  $.getJSON(service_url + '?callback=?', params, function(response) {
+    $(".mostrar").empty();
+    var conteo = 0
+    $.each(response.result,function(i, result){
+      conteo +=1
+    var obj={text:result['name']}.text;
+    var insertar='<tr>'+'<td>'+conteo+'</td>'+'<td>'+obj+'</td>'+'</tr>'
+    $('.mostrar').append(insertar);
+});
+  });
+});
+});
