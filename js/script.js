9b7c829130eb2c60388494cbee356ea3d5bbb349
miltonsms <milsmsramirez@gmail.com>
2014-12-11 11:54:07 -0600

    v5

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     //Knockout Test
-        var url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=VLLLSt653h2wwu3IeYhiL9Qwyg&part=snippet%2Cid&order=date&maxResults=50";
+        var url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCRRxSTgPUY0q_YToaczc2BQ&part=snippet%2Cid&order=date&maxResults=5";
         var viewModel = {};
         $.getJSON(url, function (data) {
-            viewModel.model = ko.mapping.fromJS(data);
+            viewModel.model = data;
             ko.applyBindings(viewModel);
         });
     });
