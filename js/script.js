37b7d81ad1331741b4d352c2af1564a559cc99f0
miltonsms <milsmsramirez@gmail.com>
2014-12-12 13:15:00 -0600

    v6

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
     //Knockout Test
-        var url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCRRxSTgPUY0q_YToaczc2BQ&part=snippet%2Cid&order=date&maxResults=5";
+        var url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCRRxSTgPUY0q_YToaczc2BQ&part=snippet%2Cid&order=viewCount&maxResults=5";
         var viewModel = {};
         $.getJSON(url, function (data) {
-            viewModel.model = data;
+            viewModel.Model = data;
             ko.applyBindings(viewModel);
         });
     });
+$(document).ready(function()
+{
+$(".uno").click(function () {
+$('.ocultar ').toggle("slow");
+});
+});
+
+//vos pero espera que jueguen22617585
