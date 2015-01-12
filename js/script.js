328132d0264a83749531799cfbb4c2d45aa6d0a8
milton <milsmsramirez@gmail.com>
2015-01-12 06:00:53 -0600

    colocacion filtros

--- js/script.js
+++ js/script.js
@@ -1,60 +1,147 @@
 /**
  * @project frontend-youtube
  * @author Sergio Merida <tabarinisergio@gmail.com> 
- * Bring all the Id and then we save in UrlData with the new Url
+ * Save all the data in arrays and makes easier manage with knockout
 */
 stringUrl="";
 UrlData = "";
 viewCount = [];
-titulo = [];
-description =[];
+likeCount = [];
+commentCount = [];
+id = [];
+title = [];
+description = [];
+image = [];
+
+/**
+ * Dropdown of filters 
+*/
 $(document).ready(function() {
 	$('.dropdown-toggle').dropdown()
 });
 
 $(document).ready(function($) {
 	$.ajax({
-		url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCutAQ7OXuxEZ1Cw3ZPmPOZA&part=snippet&maxResults=10&format=json",
+		url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCutAQ7OXuxEZ1Cw3ZPmPOZA&part=snippet&maxResults=50&format=json",
 		dataType : "jsonp",
 		success : function(parsed_json) {
 	
-		for (var i = 0; i <= 9; i++) {
-			if (i==9){
+		for (var i = 0; i <= 49; i++) {
+			if (i==49){
 				stringUrl = stringUrl + parsed_json["items"][i]["id"]["videoId"];
 			}else{
 				stringUrl = parsed_json["items"][i]["id"]["videoId"]+ "%2C+" +stringUrl
-			};		
-		};
+			}; /*End of for*/		
+		};/*Succes*/
 		UrlData = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id="+stringUrl+"&maxResults=10&key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo";
 		console.log(UrlData)
 		$.ajax({
-		url: UrlData,
-		dataType : "jsonp",
-		success : function(parsed_jso) {
-			for (var e = 0; e<=9; e++) {
-				viewCount.push(parsed_jso["items"][e]["statistics"]["viewCount"])
-			};	
-	console.log(viewCount)
-		}
-				
-	});
+			url: UrlData,
+			dataType : "jsonp",
+			success : function(parsed_jso) {
+				for (var e = 0; e<=49; e++) {
+					title.push(parsed_jso["items"][e]["snippet"]["title"])
+					id.push(parsed_jso["items"][e]["id"])
+					description.push(parsed_jso["items"][e]["snippet"]["description"])		
+					viewCount.push(parsed_jso["items"][e]["statistics"]["viewCount"])
+					likeCount.push(parsed_jso["items"][e]["statistics"]["likeCount"])
+					commentCount.push(parsed_jso["items"][e]["statistics"]["commentCount"])
+					image.push(parsed_jso["items"][e]["snippet"]["thumbnails"]["default"]["url"])
+				};			
+			console.log(id)
+			console.log(title)
+			var initialData = []
+			for (var e = 0; e<=49; e++) {
+				initialData.push(({name:title[e], images:image[e], view:viewCount[e], like:likeCount[e], comment:commentCount[e]
+, ids:id[e]}))
+			};
 
+var ViewModel = function(items) {
+    this.items = ko.observableArray(items);
 
-		}
+	this.sortByName = function() {
+        this.items.sort(function(a, b) {
+            return a.name < b.name ? -1 : 1;
+        });
+        $('.videos').slideUp();  $('.videos').slideDown();
+    };
+    	this.sortByName_z_a = function() {
+        this.items.sort(function(a, b) {
+            return a.name > b.name ? -1 : 1;
+        });
+    };
+
+	this.sortByLikes = function() {
+        this.items.sort(function(c, d) {
+            return c.like > d.like ? -1 : 1;
+        });
+    };
 	
-	});
-	
-});
-jQuery(document).ready(function($) {
-$.ajax({
-url : "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=05d4b311160508e4320ec650346e45c8&format=json",
-dataType : "jsonp",
-success : function(parsed_json) {
-	titulo.push(parsed_json["items"]["snippet"]["title"])
-	id.push(parsed_json["items"]["id"])
-	description.push(parsed_json["items"]["snippet"]["description"])
-		$(".milton").append(titulo+ id);
+	this.sortByViews = function() {
+        this.items.sort(function(c, d) {
+            return c.view > d.view ? -1 : 1;
+        });
+    };
+    	this.sortByLestViews = function() {
+        this.items.sort(function(c, d) {
+            return c.view < d.view ? -1 : 1;
+        });
+    };
 
-	};
-});
-});
+	
+	this.sortByComments = function() {
+        this.items.sort(function(c, d) {
+            return c.comment < d.comment ? -1 : 1;
+        });
+    };     
+
+    	this.sortByLestComments = function() {
+        this.items.sort(function(c, d) {
+            return c.comment > d.comment ? -1 : 1;
+        });
+    };
+
+    this.gridOptions = {
+        data: this.items,
+        rowTemplate: "rowTmpl",
+        useKOTemplates: true,
+        height: 415,
+
+        columns: [ 
+            {
+                title: "Name"
+            },
+            {
+                title: "Image"   
+            },
+			{
+				title: "View Count"
+			},
+			{
+				title: "Like Count"
+			},
+			{
+				title: "Comment Count"
+			},
+        ],
+	pageable: {
+        // we don't set any DataSource here
+        pageSize: 3,
+        buttonCount: 1
+      },
+    };
+};
+
+
+ko.applyBindings(new ViewModel(initialData));
+
+			}
+		}); /*End of second ajax call*/
+		} /*end of variable with if and else*/
+
+	});/*End of first ajax call*/
+	
+});/*End of ready function*/ 
+
+
+
